0a750cb5476f78491b0a0c06ecdf8734083d391e
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-03-18 22:43:20 +0530

    routes added

--- routes/donor.js
+++ routes/donor.js
@@ -1,5 +1,5 @@
 module.exports = function(router, db){ //use db for db operations
-    router.get('/', function (req, res, next) {
+    router.get('/donor', function (req, res, next) {
         res.render('donor', { title: ' donor registration' });
     });
     return router;
--- routes/recepient.js
+++ routes/recepient.js
@@ -1,5 +1,5 @@
 module.exports = function(router, db){ //use db for db operations
-    router.get('/', function (req, res, next) {
+    router.get('/recepient', function (req, res, next) {
         res.render('recepient', { title: 'Search for a donor' });
     });
     return router;
