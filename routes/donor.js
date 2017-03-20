4cde091bdd024fcb70be5c535ef3d7add9aa7ba3
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-03-20 22:40:20 +0530

    donor insertion to db with date done

--- routes/donor.js
+++ routes/donor.js
@@ -1,8 +1,15 @@
+var donorCollection = "donors";
+
 module.exports = function(router, db){ //use db for db operations
     router.post('/donor', function (req, res, next) {
-
         console.log(req.body);
-        res.json(true);
+        db.collection(donorCollection).insertOne(Object.assign(req.body, {date: new Date()}), function (err, response) {
+            var isInserted = false;
+            if(response){
+                isInserted = true;
+            }
+            res.json(isInserted);
+        });
     });
     return router;
 };
