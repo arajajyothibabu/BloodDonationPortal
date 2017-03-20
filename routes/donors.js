c168965bd89d47d522f9d242a186788b125231b1
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-03-20 22:51:11 +0530

    route with donors from db

--- routes/donors.js
+++ routes/donors.js
@@ -1,7 +1,23 @@
+var donorCollection = "donors";
 module.exports = function(router, db){ //use db for db operations
     router.post('/donors', function (req, res, next) {
-        console.log(req.body);
-        res.json({performance: {total: 10000, fetched: 500, time: 2}, donors: []});
+        var body = req.body;
+        console.log(body);
+        db.collection(donorCollection).find({
+            "location": {
+                $near: body.location,
+                $maxDistance: body.radius / 1000 //into KM
+            },
+            group: body.group
+        }).toArray(function (err, response) {
+            var donors = [];
+            if(response){
+                donors = response;
+            }
+            console.log("Response Donors -> ", donors);
+            //TODO: query performance
+            res.json({performance: {total: 10000, fetched: 500, time: 2}, donors: donors});
+        });
     });
     return router;
 };
