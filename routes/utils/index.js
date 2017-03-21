dc96c012568d4aa471e733fba49ff6a5a1cc6a48
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-03-21 23:14:00 +0530

    group map util

---
+++ routes/utils/index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by jyothi on 21/3/17.
+ */
+module.exports = {
+
+    bloodGroupsCompatibilityMap : {
+
+        "A+": ["A+","A-","O+","O-"],
+
+        "A-": ["A-","O-"],
+
+        "B+": ["B+","B-","O+","O-"],
+
+        "B-": ["B-","O-"],
+
+        "AB+": ["A+","A-","B+","B-","AB+","AB-","O+","O-"],
+
+        "AB-": ["A-","B-","AB-","O-"],
+
+        "O+": ["O+","O-"],
+
+        "O-": ["O-"]
+    }
+
+}
