5d9c624ff43f7bfbdbb89b4e22880672a33d6ddf
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-03-27 10:39:20 +0530

    query performance added

--- routes/donors.js
+++ routes/donors.js
@@ -4,7 +4,7 @@ var donorCollection = "donors";
 module.exports = function(router, db){ //use db for db operations
     router.post('/donors', function (req, res, next) {
         var body = req.body;
-        console.log(body);
+        //console.log(body);
         var query = {
             "location": {
                 $near: body.location,
@@ -12,16 +12,25 @@ module.exports = function(router, db){ //use db for db operations
             },
             group: {'$in': bloodGroupsCompatibilityMap[body.group]} //checking for compatible groups
         };
-        console.log(query);
-        db.collection(donorCollection).find(query).toArray(function (err, response) {
+        //console.log(query);
+        var cursor = db.collection(donorCollection).find(query);
+        var performanceCursor = db.collection(donorCollection).find(query); //FIXME: get both result and performance from single query
+        cursor.toArray(function (err, response) {
             var donors = [];
             if(response){
                 donors = response;
             }
             console.log("Response Donors -> ", donors);
-            //TODO: query performance
-            res.json({performance: {total: 10000, fetched: 500, time: 2}, donors: donors});
-        });
+            performanceCursor.explain(function (error, explanation) {
+                if(explanation){
+                    var performance = explanation.executionStats;
+                    res.json({performance: {total: performance.totalDocsExamined, fetched: performance.nReturned, time: performance.executionTimeMillis / 1000}, donors: donors});
+                }
+                if(error){
+                    res.json({performance: {total: "NA", fetched: "NA", time: "NA"}, donors: donors});
+                }
+            });
+        })
     });
     return router;
 };
